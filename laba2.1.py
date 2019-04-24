ff0f2443ff4298a2fb82e98992f223f41883fc36
ileymer <49707389+ileymer@users.noreply.github.com>
2019-04-24 08:18:29 +0300

    Update laba2.1.py

--- laba2.1.py
+++ laba2.1.py
@@ -1,11 +1,10 @@
 str = input()
 lst = str.split()
 m=len(lst)
-
+g=[]
 for i in range(m):
     t=len(lst[i])
     if t>5 and lst[i][5]!=",":
-        print(lst[i])
-
-
+        g.append(lst[i])
+print(g)
 
